04ec278bea5fb12c56f5ae065cc2b87cba2adc9c
olegeremenko <leg0@tut.by>
2022-12-01 13:11:12 +0300

    feat: solve CLI tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    let res = [];
+    const args = process.argv.slice(2);
+    for (let i = 0; i < args.length; i+=2) {
+        let propName = args[i].substring(2);
+        let value = args[i + 1];
+        res.push(propName + ' is ' + value);
+    }
+    console.log(res.join(', '));
 };
 
 parseArgs();
