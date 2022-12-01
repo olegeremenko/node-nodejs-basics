bc0e0ac88346d7c6b2fb1c4e3ad2f9942c2c5b85
olegeremenko <leg0@tut.by>
2022-12-01 15:56:27 +0300

    feat: solve zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const baseDir = dirname(fileURLToPath(import.meta.url));
+const srcFile = baseDir + '/files/fileToCompress.txt';
+const dstFile = baseDir + '/files/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const inputStream = createReadStream(srcFile);
+    const outStream = createWriteStream(dstFile);
+    inputStream.pipe(gzip).pipe(outStream);
 };
 
 await compress();
