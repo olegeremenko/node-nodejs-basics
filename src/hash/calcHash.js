0989f2826430dfb08ecdfc7ccb617b54145c8869
olegeremenko <leg0@tut.by>
2022-12-01 13:11:34 +0300

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from 'crypto';
+import { readFile } from 'node:fs/promises';
+
+const hashSha256 = async (string) => {
+    return createHash('sha256').update(string).digest('hex');
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    const content = await readFile('./src/hash/files/fileToCalculateHashFor.txt', 'utf8');
+    console.log(await hashSha256(content));
 };
 
 await calculateHash();
