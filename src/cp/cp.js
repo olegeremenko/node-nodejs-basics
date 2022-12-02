63134b8c0327a40d216208a758c3de518d23b356
olegeremenko <leg0@tut.by>
2022-12-02 18:40:18 +0300

    feat: solve child-process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const baseDir = dirname(fileURLToPath(import.meta.url));
+    const child = spawn(`node`, [`${baseDir}/files/script.js`, ...args]);
+
+    child.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    process.stdin.pipe(child.stdin);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.splice(2));
